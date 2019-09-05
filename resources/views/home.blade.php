5fcae14c1465c0b9b8ae1f1c5de6ea88b01fdf28
= <dxwebmaster@gmail.com>
2019-09-05 10:16:37 -0500

    Updated home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -75,7 +75,7 @@
     <div class="row justify-content-center mt-5">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Dashboard VUE</div>
+                    <div class="card-header">Dashboard VUE1</div>
     
                     <div class="card-body">                                      
                         
