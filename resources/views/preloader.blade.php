ec43eb7abc9fc33e1342c92ff19fcbb37eac1e5a
= <dxwebmaster@gmail.com>
2019-09-10 05:09:00 -0500

    added h5

--- resources/views/preloader.blade.php
+++ resources/views/preloader.blade.php
@@ -5,6 +5,7 @@
     <div class="row">
         <div class="col-lg-12">
             <h1>THIS IS A PRELOADER</h1>
+            <h5>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis aliquid, itaque et enim nobis perspiciatis corporis dolor debitis qui perferendis natus quo quam, animi dignissimos excepturi tempore soluta deleniti nam?</h5>
         </div>
     </div>
 </div>
